15c74cd12584bca0336fd65a06484495d6505049
Digvijay557 <digvijaay55@gmail.com>
2026-06-08 10:03:01 +0530

    added axios

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import axios from "axios";
 export default function Home() {
   // const products = [
   //   {
