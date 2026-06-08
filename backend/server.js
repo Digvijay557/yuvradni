65bcf16815179c984a5ab20a52d73cefcd9c65ec
Digvijay557 <digvijaay55@gmail.com>
2026-06-08 10:42:10 +0530

    remove duplicate cors config

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const userRoutes = require('./routes/userRoutes')
 const productRoutes = require('./routes/productRoutes')
 const orderRoutes = require('./routes/orderRoutes')
 const app = express()
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 
